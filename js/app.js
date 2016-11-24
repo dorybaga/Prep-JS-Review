aae9972c21e3a6961e919fe138f08deaf5ef69de
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2016-11-23 18:20:07 -1000

    pau ex8

--- js/app.js
+++ js/app.js
@@ -76,11 +76,14 @@ Declare a variable named india and assign the value of 3 popular tourist attract
 Console log the variable.*/
 
 var india = ["taj mahal", "ganges", "rani ki vav"];
-console.log(india);
+// console.log(india);
 
 /*8. Adding new elements to an array.
 Add another popular tourist attraction to the india variable from position 1 of the index.
 (hint, it is not the push() method). Console log the variable.*/
+
+india.splice(1, 0, "lotus temple");
+// console.log(india);
 
 /*9. Removing the last element from an array.
 Using the same india variable, remove the last element from the array. Console log the variable. */
