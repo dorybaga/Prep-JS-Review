616f0a54dcb87515d54ef91495404dec38163f7b
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2016-11-23 18:22:33 -1000

    pau ex9

--- js/app.js
+++ js/app.js
@@ -88,6 +88,9 @@ india.splice(1, 0, "lotus temple");
 /*9. Removing the last element from an array.
 Using the same india variable, remove the last element from the array. Console log the variable. */
 
+india.splice(india.length-1, 1);
+// console.log(india);
+
 /*10. Removing an element from an array into a new array. 
 Removethe first two elements in the snacks array and return a new array.
 Console log the new variable. */
